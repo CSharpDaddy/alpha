6479ba76e07eaed9638ce3107897eb31dea16486
CSharpDaddy <91020265+CSharpDaddy@users.noreply.github.com>
2021-11-01 17:01:57 -0230

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -1,70 +1,37 @@
--- Gui to Lua
--- Version: 3.2
-
--- Instances:
-
-local ScreenGui = Instance.new("ScreenGui")
-local Frame = Instance.new("Frame")
-local UIListLayout = Instance.new("UIListLayout")
-local TextButton = Instance.new("TextButton")
-
---Properties:
-
-ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
-Frame.Parent = ScreenGui
-Frame.BackgroundColor3 = Color3.fromRGB(240, 150, 24)
-Frame.BorderSizePixel = 0
-Frame.Position = UDim2.new(0.0784913376, 0, 0.0509977862, 0)
-Frame.Size = UDim2.new(0, 147, 0, 259)
-
-UIListLayout.Parent = Frame
-UIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Right
-UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
-
-TextButton.Parent = Frame
-TextButton.BackgroundColor3 = Color3.fromRGB(41, 41, 41)
-TextButton.BackgroundTransparency = 1.000
-TextButton.BorderSizePixel = 0
-TextButton.Position = UDim2.new(0.755102038, 0, 0, 0)
-TextButton.Size = UDim2.new(0, 36, 0, 25)
-TextButton.Font = Enum.Font.SourceSans
-TextButton.Text = "X"
-TextButton.TextColor3 = Color3.fromRGB(255, 255, 255)
-TextButton.TextSize = 14.000
-
 local Library = {}
 
-function Library:CreateMain()
+function Library:CreateMain()	
 	local ScreenGui = Instance.new("ScreenGui")
 	local Frame = Instance.new("Frame")
 	local UIListLayout = Instance.new("UIListLayout")
-	local TextButton = Instance.new("TextButton")
+
 
 	ScreenGui.Parent = game:GetService("CoreGui")
 	ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
+	
 	Frame.Parent = ScreenGui
-	Frame.BackgroundColor3 = Color3.new(240, 150, 24)
-	Frame.Position = UDim2.new(0.021, 0, 0.033, 0)
-	Frame.Size = UDim2.new(0, 147,0, 259)
-
+	Frame.BackgroundColor3 = Color3.new(0.168627, 0.168627, 0.168627)
+	Frame.Position = UDim2.new(0.747491598, 0, 0.22850123, 0)
+	Frame.Size = UDim2.new(0, 267, 0, 158)
+	
 	UIListLayout.Parent = Frame
 	UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+	
+	local GamerLibrary = {}
+	
+	function GamerLibrary:NewButton(name)
+		local TextButton = Instance.new("TextButton")
+			
+		TextButton.Parent = Frame
+		TextButton.BackgroundColor3 = Color3.new(1, 1, 1)
+		TextButton.Size = UDim2.new(0, 200, 0, 38)
+		TextButton.Font = Enum.Font.SourceSans
+		TextButton.Text = name
+		TextButton.TextColor3 = Color3.new(0, 0, 0)
+		TextButton.TextSize = 14
+	end
+	
+	return GamerLibrary;
 end
-
-function Library:CreateButton(name)
-	local TextButton = Instance.new("TextButton")
-
-	TextButton.Parent = Frame
-	TextButton.BackgroundColor3 = Color3.new(41, 41, 41)
-	TextButton.Size = UDim2.new(0, 147, 0, 25)
-	TextButton.Font = Enum.Font.Cartoon
-	TextButton.Text = name
-	TextButton.TextColor3 = Color3.new(0, 0, 0)
-	TextButton.TextSize = 14
-	TextButton.BackgroundTransparency = 1
-end 
 
 return Library;
